1f3a9ebe915cb7f7499df75fbc747d20f5a6fe2d
satfinder8 <satfinder8@gmail.com>
2023-09-15 13:28:00 +0300

    Initiate project Aquaworld

---
+++ src/main/java/com/naumencourse/aquaworld/AquaworldApplication.java
@@ -0,0 +1,21 @@
+package com.naumencourse.aquaworld;
+
+import org.h2.tools.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class AquaworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaworldApplication.class, args);
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	}
+}
---
+++ src/test/java/com/naumencourse/aquaworld/AquaworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naumencourse.aquaworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
