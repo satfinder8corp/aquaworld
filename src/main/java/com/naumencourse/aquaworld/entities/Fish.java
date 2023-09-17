ce21b6a752313b565ebcdea456227332a8d12673
Filippova Ekaterina <lwrow220816@gmail.com>
2023-09-17 14:08:15 +0300

    edit "Fish" in entity folder

--- src/main/java/com/naumencourse/aquaworld/entities/Fish.java
+++ src/main/java/com/naumencourse/aquaworld/entities/Fish.java
@@ -1,15 +1,43 @@
 package com.naumencourse.aquaworld.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
+
+//@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "fish")
 public class Fish {
+
     @Id
+    @Column(name = "id")
     private Long fishId;
 
+    @Column(name = "name")
     private String fishName;
 
+    @Column(name = "salty_water")
+    private Boolean isWaterSalty;
+
+    @Column(name = "min_HP")
+    private Integer minHP;
+
+    @Column(name = "max_HP")
+    private Integer maxHP;
+
+    @Column(name = "min_temperature")
+    private Integer minTemperature;
+
+    @Column(name = "max_temperature")
+    private Integer maxTemperature;
+
+    @ManyToMany
+    @JoinTable(name = "fish_aquariums",
+            joinColumns = @JoinColumn(name = "fish_id", referencedColumnName = "aquariums_id"))
+    private List<Aquarium> aquariumList = new java.util.ArrayList<>();
 }
