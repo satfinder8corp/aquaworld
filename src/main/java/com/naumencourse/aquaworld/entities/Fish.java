2bde0932ef6777cfdac54b6d3f1a63dd5f0a3f78
Filippova Ekaterina <lwrow220816@gmail.com>
2023-10-05 17:32:44 +0300

    added comments & edited fields in Fish entity

--- src/main/java/com/naumencourse/aquaworld/entities/Fish.java
+++ src/main/java/com/naumencourse/aquaworld/entities/Fish.java
@@ -18,24 +18,40 @@ public class Fish {
     @Column(name = "id")
     private Long fishId;
 
+    // наименование типа рыбы (неон, скалярия и т.п.)
     @Column(name = "name")
     private String fishName;
 
+    // маркер соленая вода или пресная нужна рыбе
+    // true - соленая, false - пресная
     @Column(name = "salty_water")
     private Boolean isWaterSalty;
 
+    // маркер дружелюбия рыбки
+    @Column(name = "is_friendly")
+    private Boolean isFriendly;
+
+    // минимальная жесткость воды
     @Column(name = "min_HP")
     private Integer minHP;
 
+    // максимальная жесткость воды
     @Column(name = "max_HP")
     private Integer maxHP;
 
+    // минимальная температура воды
     @Column(name = "min_temperature")
     private Integer minTemperature;
 
+    // максимальная температура воды
     @Column(name = "max_temperature")
     private Integer maxTemperature;
 
+    // необходимый объем воды в литрах на одну рыбу данного вида
+    @Column(name = "volume")
+    private Double minVolume;
+
+    // список аквариумов, в котором есть этот вид рыб
     @ManyToMany
     @JoinTable(name = "fish_aquariums",
             joinColumns = @JoinColumn(name = "fish_id", referencedColumnName = "aquariums_id"))
