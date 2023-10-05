1600b7a746f8833c7379dea27e4e6735c5610c82
Filippova Ekaterina <lwrow220816@gmail.com>
2023-10-06 00:34:47 +0300

    put all into comments

--- src/main/java/com/naumencourse/aquaworld/controllers/FishController.java
+++ src/main/java/com/naumencourse/aquaworld/controllers/FishController.java
@@ -1,8 +1,27 @@
-package com.naumencourse.aquaworld.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class FishController {
-
-}
+//package com.naumencourse.aquaworld.controllers;
+//
+//import com.naumencourse.aquaworld.entities.Fish;
+//import com.naumencourse.aquaworld.services.FishService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class FishController {
+//
+//    private final FishService fishService;
+//
+//    @PostMapping(value = "/fish/add")
+//    public String create(Fish fish) {
+//        fishService.create(fish);
+//        return "redirect:/fish";
+//    }
+//
+//    @PostMapping(value = "/fish/{id}")
+//    public String delete(@PathVariable(name = "id") Long id) {
+//        fishService.delete(id);
+//        return "redirect:/fish";
+//    }
+//}
--- src/main/java/com/naumencourse/aquaworld/entities/Aquarium.java
+++ src/main/java/com/naumencourse/aquaworld/entities/Aquarium.java
@@ -1,26 +1,26 @@
-package com.naumencourse.aquaworld.entities;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.List;
-import java.util.UUID;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "aquarium")
-public class Aquarium {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id", nullable = false)
-    private UUID id;
-
-    @ManyToMany
-    private List<Fish> fishList;
-
-//    @ManyToOne
-//    private User user;
-}
+//package com.naumencourse.aquaworld.entities;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+////@Entity
+////@Table(name = "aquarium")
+//public class Aquarium {
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.UUID)
+////    @Column(name = "id", nullable = false)
+//    private UUID id;
+//
+////    @ManyToMany
+//    private List<Fish> fishList;
+//
+////    @ManyToOne
+////    private User user;
+//}
