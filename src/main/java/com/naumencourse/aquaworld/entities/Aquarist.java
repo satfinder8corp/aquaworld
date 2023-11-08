691d68084d3ab8f5ccc1e7056d4dcab89c47792e
Ramil A <ramil.alyukov0@gmail.com>
2023-11-08 22:33:40 +0300

    add Aquarist, AquaristRepository, deleted 2 empty files.

---
+++ src/main/java/com/naumencourse/aquaworld/entities/Aquarist.java
@@ -0,0 +1,42 @@
+package com.naumencourse.aquaworld.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.*;
+
+@Entity
+@Table(name = "aquarists")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Aquarist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID AquaristId;
+
+    private String  username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private Date dateOfRegistry;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "aquarist_role", joinColumns = @JoinColumn(name = "aquarist_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> role = new HashSet<>();
+
+    @OneToMany(mappedBy = "owner")
+    private List<Aquarium> aquariums;
+
+    @ElementCollection
+    @CollectionTable(name = "aquarium_fish_population")
+    @MapKeyJoinColumn(name = "fish_id")
+    @Column(name = "population_count")
+    private Map<Fish, Integer> population;
+}
---
+++ src/main/java/com/naumencourse/aquaworld/repositories/AquaristRepository.java
@@ -0,0 +1,11 @@
+package com.naumencourse.aquaworld.repositories;
+
+import com.naumencourse.aquaworld.entities.Aquarist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AquaristRepository extends JpaRepository<Aquarist, UUID> {
+    Optional<Aquarist> findByEmail(String email);
+}
