be144c93e8c27636fb9897916ef9a46590394fe2
Ramil A <ramil.alyukov0@gmail.com>
2023-11-08 22:41:11 +0300

    add Fish and FishRepository

--- src/main/java/com/naumencourse/aquaworld/entities/Fish.java
+++ src/main/java/com/naumencourse/aquaworld/entities/Fish.java
@@ -1,59 +1,60 @@
-//package com.naumencourse.aquaworld.entities;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.util.List;
-//
-////@Data
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Entity
-//@Table(name = "fish")
-//public class Fish {
-//
-//    @Id
-//    @Column(name = "id")
-//    private Long fishId;
-//
-//    // наименование типа рыбы (неон, скалярия и т.п.)
-//    @Column(name = "name")
-//    private String fishName;
-//
-//    // маркер соленая вода или пресная нужна рыбе
-//    // true - соленая, false - пресная
-//    @Column(name = "salty_water")
-//    private Boolean isWaterSalty;
-//
-//    // маркер дружелюбия рыбки
-//    @Column(name = "is_friendly")
-//    private Boolean isFriendly;
-//
-//    // минимальная жесткость воды
-//    @Column(name = "min_HP")
-//    private Integer minHP;
-//
-//    // максимальная жесткость воды
-//    @Column(name = "max_HP")
-//    private Integer maxHP;
-//
-//    // минимальная температура воды
-//    @Column(name = "min_temperature")
-//    private Integer minTemperature;
-//
-//    // максимальная температура воды
-//    @Column(name = "max_temperature")
-//    private Integer maxTemperature;
-//
-//    // необходимый объем воды в литрах на одну рыбу данного вида
-//    @Column(name = "volume")
-//    private Double minVolume;
-//
-//    // список аквариумов, в котором есть этот вид рыб
-////    @ManyToMany
-////    @JoinTable(name = "fish_aquariums",
-////            joinColumns = @JoinColumn(name = "fish_id", referencedColumnName = "aquariums_id"))
-////    private List<Aquarium> aquariumList = new java.util.ArrayList<>();
-//}
+package com.naumencourse.aquaworld.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "fish")
+public class Fish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID fishId;
+
+    @Column(name = "name",unique = true)
+    private String fishName;
+
+    // маркер соленая вода или пресная нужна рыбе
+    // true - соленая, false - пресная
+    @Column(name = "salty_water")
+    private Boolean isWaterSalty;
+
+    // маркер дружелюбия рыбки
+    @Column(name = "is_friendly")
+    private Boolean isFriendly;
+
+    // минимальная жесткость воды
+    @Column(name = "min_HP")
+    private Integer minHP;
+
+    // максимальная жесткость воды
+    @Column(name = "max_HP")
+    private Integer maxHP;
+
+    // минимальная температура воды
+    @Column(name = "min_temperature")
+    private Integer minTemperature;
+
+    // максимальная температура воды
+    @Column(name = "max_temperature")
+    private Integer maxTemperature;
+
+    // необходимый объем воды в литрах на одну рыбу данного вида
+    @Column(name = "volume")
+    private Double minVolume;
+
+    // список аквариумов, в котором есть этот вид рыб
+//    @ManyToMany
+//    @JoinTable(name = "fish_aquariums",
+//            joinColumns = @JoinColumn(name = "fish_id", referencedColumnName = "aquariums_id"))
+//    private List<Aquarium> aquariumList = new java.util.ArrayList<>();
+}
--- src/main/java/com/naumencourse/aquaworld/repositories/FishRepository.java
+++ src/main/java/com/naumencourse/aquaworld/repositories/FishRepository.java
@@ -1,9 +1,9 @@
-//package com.naumencourse.aquaworld.repositories;
-//
-//import com.naumencourse.aquaworld.entities.Fish;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.UUID;
-//
-//public interface FishRepository extends JpaRepository<Fish, UUID> {
-//}
+package com.naumencourse.aquaworld.repositories;
+
+import com.naumencourse.aquaworld.entities.Fish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FishRepository extends JpaRepository<Fish, UUID> {
+}
